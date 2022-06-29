a0009a5c1a9857e3893bb21bf6a24ff29333616b
coder666s <91024517+coder666s@users.noreply.github.com>
2022-06-29 14:31:09 +0300

    финал
    1

--- index.js
+++ index.js
@@ -9,8 +9,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Отправляем index.html, когда пользователи получают доступ к
 // корневому каталог с использованием res.sendFile()
 app.get('/', (req, res) => {
-  res.send({ message: 'Васильев Михаил! 201-324' });
   res.sendFile(`${__dirname}/public/index.html`);
 });
+// Отправка данных
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({
+  extended: false,
+});
+app.get('/register', urlencodedParser, function (
+  request,
+  response
+) {
+  response.sendFile(__dirname + '/public/index.html')
+});
+app.post('/register', urlencodedParser, function (
+  request,
+  response
+) {
+  if (!request.body) return response.sendStatus(400)
+  console.log(request.body)
+  response.send(
+    `${request.body.userName} - ${request.body.userAge}`)}); // сообщение
+app.get('/', function (request, response) {
+  response.send('Главная страница')
+});
 
+// Порт
 app.listen(5500, ()=>console.log("Сервер запущен..."));
--- public/app.js
+++
@@ -1,5 +0,0 @@
-app.post("/", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
-  });
