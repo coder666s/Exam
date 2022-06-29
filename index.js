142575241fec1d29c0d18f47c81cef7b855ba25f
coder666s <91024517+coder666s@users.noreply.github.com>
2022-06-29 12:28:15 +0300

    add server
    node js exspress

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
+
 // Укажем директорию в которой будут лежать наши файлы
-//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Отправляем index.html, когда пользователи получают доступ к
 // корневому каталог с использованием res.sendFile()
 app.get('/', (req, res) => {
-  //res.sendFile(__dirname + '/index.html')
   res.send({ message: 'Васильев Михаил! 201-324' });
+  res.sendFile(`${__dirname}/public/index.html`);
 });
-
-
 
 app.listen(5500, ()=>console.log("Сервер запущен..."));
---
+++ public/app.js
@@ -0,0 +1,5 @@
+app.post("/", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+  });
